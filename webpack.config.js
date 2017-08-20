8ffed8bf1de472971e57543d74a115f06bb2545f
htolgasahin <htolgasahin@mgmail.com>
2017-08-20 18:37:15 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+let path = require("path");
+const Aliases = require('./webpack.aliases.js')
+module.exports = {
+  target: "web",
+  entry: {
+    App: "./src/App.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].build.js",
+    publicPath: "dist"
+  },
+  resolve:{alias:Aliases},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env", "react"]
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ]
+  },
+  devServer: {
+    compress: true,
+    contentBase: ".",
+    port: 8080,
+    host: "0.0.0.0",
+    historyApiFallback: {
+      index: "index.html"
+    },
+    https: process.env.production,
+    headers: {
+      "franx-environment": process.env.production
+        ? process.env.production
+        : "development"
+    }
+  }
+};
